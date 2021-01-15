fbbbc6dfc40e8df1ea9a686079664e179bcebe8f
Brendan Retter <brendan.retter@gmail.com>
2021-01-15 12:20:14 +0000

    Added docstrings Removed unnecessary functions and variables.

--- hexpuzzle.py
+++ hexpuzzle.py
@@ -1,73 +1,11 @@
+"""
+Code for solving a hexagonal puzzle.
+Puzzle pieces are labelled with the numbers from 1 to 19 and need to fit into 
+a hexagonal grid such that every line adds up to 38. 
+"""
+
 import numpy as np
-import matplotlib.pyplot as plt
-#from mpl_toolkits.mplot3d import Axes3D
 
-def toint(array,coord):
-    return int(array[tuple(coord)])
-
-def pa(array,coords):
-    vals = []
-    for c in coords:
-        vals.append(toint(array,c))
-    #print(vals)
-    print('    {:2d}  {:2d}  {:2d}    '.format(vals[0],vals[1],vals[2]))
-    print('  {:2d}  {:2d}  {:2d}  {:2d}  '.format(vals[3],vals[4],vals[5],vals[6]))
-    print('{:2d}  {:2d}  {:2d}  {:2d} {:2d}'.format(vals[7],vals[8],vals[9],vals[10],vals[11]))
-    print('  {:2d}  {:2d}  {:2d}  {:2d}  '.format(vals[12],vals[13],vals[14],vals[15]))
-    print('    {:2d}  {:2d}  {:2d}    '.format(vals[16],vals[17],vals[18]))
-    return vals
-
-def break_check(array):
-    #returns true if solution incorrect
-    if (np.sum(array,axis=(0,1)) > 38).any() or (np.sum(array,axis=(1,2)) > 38).any() or (np.sum(array,axis=(0,2)) > 38).any():
-        return True
-    else:
-        return False
-def solvcheck(array):
-    if (np.sum(array,axis=(0,1)) == 38).all() and (np.sum(array,axis=(1,2)) == 38).all() and (np.sum(array,axis=(0,2)) == 38).all():
-        return True
-    else:
-        return False
-                                                                                                                    
-def solve_values(array,numbers,pindex):
-    global loops
-    global debug
-    #print(numbers)
-    numbers=numbers[:]
-    loops +=1
-    pindex +=1
-    coord = tuple(coords[pindex])
-    for n in numbers:
-        if loops > looplim:
-            break                
-
-        #check solution
-        if solvcheck(array):
-            return array
-        array[coord] = n
-        ## if any lines sum to greater than 38, return False
-        if break_check(array):
-            continue
-        else:
-            nums = numbers[:]
-            nums.remove(n)
-            if loops > looplim:
-                print('hi')
-                print(nums)
-                break
-            array = solve_values(array,nums,pindex)
-    if not solvcheck(array):
-        if not loops > looplim:
-            array[coord] = 0
-    return array
-
-x = np.array([0,0,0,1,1,1,1,2,2,2,2,2,3,3,3,3,4,4,4])
-y = np.concatenate([np.arange(3),np.arange(4),np.arange(5),np.arange(1,5),np.arange(2,5)])
-z = np.array([2,3,4,1,2,3,4,0,1,2,3,4,0,1,2,3,0,1,2])
-coords = np.vstack((x,y,z)).T
-
-
-        
 class board(object):
     #initialise board
     values = range(19,0,-1)
@@ -111,7 +49,7 @@ class board(object):
         vals = []
         for c in self.coords:
             vals.append(toint(array,c))
-        #print(vals)
+
         print('    {:2d}  {:2d}  {:2d}    '.format(vals[0],vals[1],vals[2]))
         print('  {:2d}  {:2d}  {:2d}  {:2d}  '.format(vals[3],vals[4],vals[5],vals[6]))
         print('{:2d}  {:2d}  {:2d}  {:2d} {:2d}'.format(vals[7],vals[8],vals[9],vals[10],vals[11]))
@@ -119,7 +57,7 @@ class board(object):
         print('    {:2d}  {:2d}  {:2d}    '.format(vals[16],vals[17],vals[18]))
         
     def solve_check(self):
-        array = board.p_array
+        array = self.p_array
         if (np.sum(array,axis=(0,1)) == 38).all() and (np.sum(array,axis=(1,2)) == 38).all() and (np.sum(array,axis=(0,2)) == 38).all():
             return True
         else:
@@ -128,29 +66,30 @@ class board(object):
     def solve(self,hindex):
         hex1 = self.hexlist[hindex]
         valtest = hex1.get_values(self)
-        #print(valtest)
+
         if valtest is False:
             return False
         numbers = hex1.vals
         for n in numbers:
-            #print('{}:{}'.format(hindex,n))
 
             if self.loops > self.looplim:
                 return True
             hextest = hex1.set_value(n,self)
             self.loops +=1
+            
             #if puzzle is solved stop working
             if self.solve_check():
                 return True
             hextest = self.solve(hindex+1)
             if hextest is False:
                 hex1.reset(self)
+                
             #if puzzle is solved stop working
             if self.solve_check():
                 return True
             if self.loops > self.looplim:
                 return True
-            #hex1.reset()
+
         return False
             
     def set_hex(self,hex,num):
@@ -190,9 +129,7 @@ class hex(board):
         self.j = j
         self.k = k
         self.val=0
-    #def breakcheck(self):
-    #    if np.sum(np.sum(p_array[self.i]),38-np.sum(p_array[self.j]),38-np.sum(p_array[self.k]))
-    #    return None
+
     def vals(self):
         print(self.values)
     def pop_val(self,val):
@@ -214,7 +151,7 @@ class hex(board):
         if len(set(biggest)) > 1:
             return False
         elif len(set(biggest)) == 1:
-            #print(biggest)
+
             tests = list(set(biggest)&set(initial))
             if len(tests) == 0:
                 return False
@@ -248,10 +185,14 @@ class hex(board):
         board.p_array[self.i,self.j,self.k] = 0
         
 
-b = board(True,130000)
-a = b.hexlist
-c = a[0]
-c.get_values(b)
+def toint(array,coord):
+    """
+    Return integer at coordinate in array
+    """
+    return int(array[tuple(coord)])
+     
 
+
+b = board(True,130000)
 b.solve(0)
 b.pa()
